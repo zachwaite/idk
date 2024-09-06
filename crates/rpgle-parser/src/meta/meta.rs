dc51fd38d49ee9b84fed8eff81a4d33a795c8bb6
Zach Waite <zach@waiteperspectives.com>
2024-09-06 18:59:58 -0400

    [IMP] rpgle-parser: factor hlgroup and digs out of Meta

--- crates/rpgle-parser/src/meta/meta.rs
+++ crates/rpgle-parser/src/meta/meta.rs
@@ -1,14 +1,11 @@
 use super::position::Position;
 use super::span::Span;
-use super::{diagnostic::Diagnostic, Hlgroup};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Meta {
     pub span: Span,
     pub text: String,
-    pub digs: Vec<Diagnostic>,
-    pub hlgroup: Hlgroup,
 }
 
 impl From<(Position, &[char])> for Meta {
@@ -18,13 +15,6 @@ impl From<(Position, &[char])> for Meta {
         let text = chars.iter().collect::<String>();
         let end = Position::from((start.row, start.col + chars.len()));
         let span = Span { start, end };
-        let digs = vec![];
-        let hlgroup = Hlgroup::Normal;
-        Self {
-            span,
-            text,
-            digs,
-            hlgroup,
-        }
+        Self { span, text }
     }
 }
--- crates/rpgle-parser/src/meta/mod.rs
+++ crates/rpgle-parser/src/meta/mod.rs
@@ -1,13 +1,11 @@
 mod array;
 pub mod diagnostic;
-pub mod hlgroup;
 pub mod meta;
 pub mod position;
 pub mod span;
 
 pub use array::pluck_array3;
 pub use diagnostic::{Diagnostic, DiagnosticLevel};
-pub use hlgroup::Hlgroup;
 pub use meta::Meta;
 pub use position::Position;
 pub use span::Span;
