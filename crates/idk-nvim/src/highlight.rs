36f0eef81f6d5dbc5746266ce029055419036a4b
Zach Waite <zach@waiteperspectives.com>
2024-09-12 14:28:04 -0400

    [IMP] idk-nvim: add AST/CST src field to highlight.txt debug file

--- crates/idk-nvim/src/highlight.rs
+++ crates/idk-nvim/src/highlight.rs
@@ -1,4 +1,4 @@
-use rpgle_parser::{highlight_cst, Span, CST};
+use rpgle_parser::{highlight_ast, highlight_cst, Span, AST, CST};
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::fmt;
@@ -10,18 +10,20 @@ pub struct HighlightMeta {
     pub end_row: usize,
     pub end_col: usize,
     pub hl_group: String,
+    pub src: String,
 }
 
 impl fmt::Display for HighlightMeta {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let s = format!(
-            "({}, {}) -> ({}, {}): {}",
-            self.start_row, self.start_col, self.end_row, self.end_col, self.hl_group
+            "({}, {}) -> ({}, {}): {}, src={}",
+            self.start_row, self.start_col, self.end_row, self.end_col, self.hl_group, self.src,
         );
         write!(f, "{}", s)
     }
 }
 
+// deprecate
 impl From<(Span, String)> for HighlightMeta {
     fn from(value: (Span, String)) -> Self {
         Self {
@@ -30,6 +32,20 @@ impl From<(Span, String)> for HighlightMeta {
             end_row: value.0.end.row,
             end_col: value.0.end.col,
             hl_group: value.1.to_string(),
+            src: "".to_string(),
+        }
+    }
+}
+
+impl From<(Span, &str, &str)> for HighlightMeta {
+    fn from(value: (Span, &str, &str)) -> Self {
+        Self {
+            start_row: value.0.start.row,
+            start_col: value.0.start.col,
+            end_row: value.0.end.row,
+            end_col: value.0.end.col,
+            hl_group: value.1.to_string(),
+            src: value.2.to_string(),
         }
     }
 }
@@ -40,10 +56,21 @@ pub fn highlight_all(txt: &str) -> Vec<HighlightMeta> {
         if env::var("DEBUG").is_ok() {
             let _ = std::fs::write("/tmp/cst.txt", format!("{:#?}", &cst));
         }
-        highlight_cst(cst)
+        let mut out = highlight_cst(&cst)
             .into_iter()
-            .map(|tup| HighlightMeta::from(tup))
-            .collect::<Vec<HighlightMeta>>()
+            .map(|tup| HighlightMeta::from((tup.0, tup.1.as_str(), "CST")))
+            .collect::<Vec<HighlightMeta>>();
+        let ast = AST::from(&cst);
+        if env::var("DEBUG").is_ok() {
+            let _ = std::fs::write("/tmp/ast.txt", format!("{:#?}", &ast));
+        }
+        out.append(
+            &mut highlight_ast(ast)
+                .into_iter()
+                .map(|tup| HighlightMeta::from((tup.0, tup.1.as_str(), "AST")))
+                .collect::<Vec<HighlightMeta>>(),
+        );
+        out
     } else {
         vec![]
     }
