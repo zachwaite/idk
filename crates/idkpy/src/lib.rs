5f2250a9386b127e7f7dd2009c2f88c6f5731c18
Zach Waite <zach@waiteperspectives.com>
2024-10-21 07:13:24 -0400

    [ADD] idkpy: python package
    A python wrapper, currently used for testing against private data

---
+++ crates/idkpy/src/lib.rs
@@ -0,0 +1,28 @@
+use pyo3::prelude::*;
+use rpgle_parser::{AST, CST};
+use serde_json;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// Parses an input string and returns a json string AST
+#[pyfunction]
+fn parse_rpgle(txt: &str) -> PyResult<String> {
+    if let Ok(cst) = CST::try_from(txt) {
+        let ast = AST::from(&cst);
+        let out = serde_json::to_string(&ast).expect("this derives serde so must unwrap");
+        return Ok(out);
+    }
+    Ok("???".to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn idkpy(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_rpgle, m)?)?;
+    Ok(())
+}
