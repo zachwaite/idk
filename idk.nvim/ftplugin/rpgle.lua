a023226e602a905cfcd93ed38d4806b78086352d
Zach Waite <zach@waiteperspectives.com>
2026-08-21 14:45:03 -0400

    [IMP] idk.nvim: comment out unused lua fns

--- idk.nvim/ftplugin/rpgle.lua
+++ idk.nvim/ftplugin/rpgle.lua
@@ -1,8 +1,8 @@
 local idk = require("idk")
 idk.setup_rpgle()
 vim.api.nvim_create_user_command("RpgleRunTools", idk.run_rpgle_tools, {})
-vim.api.nvim_create_user_command("RpgleHighlight", idk.highlight_rpgle, {})
-vim.api.nvim_create_user_command("RpgleMarkSpecs", idk.mark_rpgle, {})
+-- vim.api.nvim_create_user_command("RpgleHighlight", idk.highlight_rpgle, {})
+-- vim.api.nvim_create_user_command("RpgleMarkSpecs", idk.mark_rpgle, {})
 idk.highlight_rpgle()
 idk.mark_rpgle()
 
@@ -11,11 +11,11 @@ vim.cmd([[set ignorecase]])
 
 -- Use shift + k to grep the word under the cursor and open the quickfix list with results
 -- use the quickfix list to inspect the matches
-vim.cmd([[nnoremap K :w<CR>mk:vimgrep! /<C-R><C-W>\c/ %<CR>:cw<CR><ENTER>]])
+-- vim.cmd([[nnoremap K :w<CR>mk:vimgrep! /<C-R><C-W>\c/ %<CR>:cw<CR><ENTER>]])
 
 -- close the quickfix list with ctl + x
 -- redundant with db2.nvim
-vim.cmd([[nnoremap <C-x> :ccl<CR><ENTER>'k]])
+-- vim.cmd([[nnoremap <C-x> :ccl<CR><ENTER>'k]])
 
 --
 -- vim.cmd([[vnoremap <C-x> mk<CR>:lua require('rpgle').remove_assignment_middleman()<CR>'k]])
